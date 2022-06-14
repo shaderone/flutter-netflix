39f62096e40debc3c3f3e44bedb7b73f35ae478b
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-14 18:49:39 +0530

    Update bottomNavbar on ui interaction

--- lib/presentation/root/widgets/bottom_nav.dart
+++ lib/presentation/root/widgets/bottom_nav.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
 
+ValueNotifier<int> currentNavIndexNotifier = ValueNotifier(0);
+
 class BottomNavWidget extends StatelessWidget {
   const BottomNavWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: 0,
-      type: BottomNavigationBarType.fixed,
-      unselectedItemColor: Colors.grey,
-      selectedItemColor: Colors.white,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.video_collection), label: "New and Hot"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.emoji_emotions_outlined), label: "Fast Laughs"),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_circle_down_rounded), label: "Downloads"),
-      ],
-    );
+    return ValueListenableBuilder(
+        valueListenable: currentNavIndexNotifier,
+        builder: (BuildContext context, int newBottomNavIndex, Widget? _) {
+          return BottomNavigationBar(
+            onTap: (selectedNavIndex) =>
+                currentNavIndexNotifier.value = selectedNavIndex,
+            currentIndex: newBottomNavIndex,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.grey,
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.video_collection), label: "New and Hot"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.emoji_emotions_outlined),
+                  label: "Fast Laughs"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search), label: "Search"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.arrow_circle_down_rounded),
+                  label: "Downloads"),
+            ],
+          );
+        });
   }
 }
