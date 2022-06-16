7cfda31f7013911e3f422f81e59fd23f4488dc98
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-16 23:07:58 +0530

    Modified reusable AppBar-Widget

--- lib/presentation/common_widgets/app_bar_widget.dart
+++ lib/presentation/common_widgets/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_clone/core/colors.dart';
 
 class AppBarWidget extends StatelessWidget {
   final String appBarTitle;
@@ -8,10 +9,12 @@ class AppBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: backgroundColor,
       title: Text(
         appBarTitle,
-        style: GoogleFonts.mukta(
+        style: GoogleFonts.montserrat(
           fontWeight: FontWeight.w800,
+          fontSize: 28,
         ),
       ),
       actions: [
@@ -19,15 +22,19 @@ class AppBarWidget extends StatelessWidget {
           onPressed: () {},
           icon: const Icon(Icons.cast),
         ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.cast),
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Container(
+            width: 24,
+            height: 24,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"),
+              ),
+            ),
+          ),
         ),
-        //Container(
-        //  width: 15,
-        //  height: 15,
-        //  color: Colors.blueAccent,
-        //),
       ],
     );
   }
