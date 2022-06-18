a00468ccd1e1697aada18fd63c13a9202dcae04f
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-18 15:38:28 +0530

    Move search-field to it's own widget file

--- lib/presentation/pages/search/search_screen.dart
+++ lib/presentation/pages/search/search_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/presentation/pages/search/widgets/idle_search.dart';
+import 'package:netflix_clone/presentation/pages/search/widgets/search_field.dart';
+import 'package:netflix_clone/presentation/pages/search/widgets/search_idle.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -17,34 +17,6 @@ class SearchScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SearchField extends StatelessWidget {
-  const SearchField({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CupertinoSearchTextField(
-          prefixIcon: const Icon(
-            CupertinoIcons.search,
-            color: Colors.grey,
-            size: 24,
-          ),
-          backgroundColor: Colors.grey.shade800,
-          style: TextStyle(color: Colors.grey.shade100),
-          suffixIcon: const Icon(
-            CupertinoIcons.xmark_circle_fill,
-            color: Colors.grey,
-          ),
-        ),
-        const SizedBox(height: 20),
-      ],
     );
   }
 }
