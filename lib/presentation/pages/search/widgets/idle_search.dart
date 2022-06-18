2a8994e4385c10e718444cf766db66681183928f
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-18 14:36:09 +0530

    Update top-search UI from Listile to custom (Row)
    Reason : Listview can't have height

--- lib/presentation/pages/search/widgets/idle_search.dart
+++ lib/presentation/pages/search/widgets/idle_search.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const _tempImg =
     "https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/56v2KjBlU4XaOv9rVYEQypROD7P.jpg";
@@ -17,12 +19,12 @@ class IdleSearchScreen extends StatelessWidget {
             fontSize: 28,
           ),
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 20),
         Flexible(
           child: ListView.separated(
             shrinkWrap: true,
             itemBuilder: (context, index) => const TopSearchListTile(),
-            separatorBuilder: (context, index) => const Divider(),
+            separatorBuilder: (context, index) => const SizedBox(height: 15),
             itemCount: 25,
           ),
         ),
@@ -37,19 +39,39 @@ class TopSearchListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenDimensions = MediaQuery.of(context).size;
-    return ListTile(
-      leading: Container(
-        width: screenDimensions.width / 3,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(_tempImg),
+    return Row(
+      children: [
+        SizedBox(
+          width: screenDimensions.width / 2.75,
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: const Image(
+                image: NetworkImage(_tempImg),
+                fit: BoxFit.fill, // use this
+              ),
+            ),
           ),
         ),
-      ),
-      title: const Text("Stranger Things"),
-      trailing: const Icon(Icons.play_circle_outline_rounded),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            "Stranger Things",
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            CupertinoIcons.play_circle,
+            size: 50,
+          ),
+        )
+      ],
     );
   }
 }
