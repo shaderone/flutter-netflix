632c9f80840cc5e2e232e369ea4ea8ce7741f45f
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-24 22:34:46 +0530

    Create new widget for home-top-hero-section

--- lib/presentation/pages/home/home_screen.dart
+++ lib/presentation/pages/home/home_screen.dart
@@ -10,124 +10,134 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         physics: const BouncingScrollPhysics(),
-        children: [
+        children: const [
           Padding(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: Stack(
-              alignment: Alignment.center,
-              clipBehavior: Clip.none,
-              children: [
-                AspectRatio(
-                  aspectRatio: 2 / 2.35,
-                  child: Container(
-                    //height: MediaQuery.of(context).size.width + MediaQuery.of(context).size.width / 3,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue.shade100.withOpacity(.1),
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        alignment: Alignment.bottomCenter,
-                        image: NetworkImage(
-                          "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/A7CzMZBitf00BAtb1kJa50pWPgV.jpg",
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  left: 0,
-                  right: 0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      HeroStats(stat: "Emotional"),
-                      HeroStats(stat: "drama"),
-                      HeroStats(stat: "family"),
-                      HeroStats(stat: "thriller", isFinalStat: true),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: -50,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width -
-                        MediaQuery.of(context).size.width / 4,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const HeroActions(
-                          icon: Icons.add,
-                          iconText: "My List",
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 15),
-                            child: Row(
-                              children: const [
-                                Icon(
-                                  Icons.play_arrow,
-                                  color: Colors.black,
-                                  size: 40,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(right: 5),
-                                  child: Text(
-                                    "Play",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const HeroActions(
-                          icon: Icons.add,
-                          iconText: "My List",
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            padding: EdgeInsets.only(bottom: 50),
+            child: HeroWidget(),
           ),
-          const SizedBox(height: 20),
-          const TitledVCardListWidget(
+          SizedBox(height: 20),
+          TitledVCardListWidget(
             listTitle: "Released in the Past Year",
             listImg:
                 "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
           ),
-          const TitledVCardListWidget(
+          TitledVCardListWidget(
             listTitle: "Trending Now",
             listImg:
                 "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/stTEycfG9928HYGEISBFaG1ngjM.jpg",
           ),
-          const NumTitledVCardListWidget(
+          NumTitledVCardListWidget(
             listTitle: "Top 10 TV Shows in India Today",
             listImg:
                 "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/wZfwOIlbc81pZb1NIgN2laZQWQk.jpg",
           ),
-          const TitledVCardListWidget(
+          TitledVCardListWidget(
             listTitle: "Most Popular",
             listImg:
                 "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/gSjykY5ZuVWHK0C7k4CXZ5566No.jpg",
           ),
-          const TitledVCardListWidget(
+          TitledVCardListWidget(
             listTitle: "Tense Dramas",
             listImg:
                 "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/nShEY0JnMOsvdhEnmYvL9mowIKz.jpg",
           ),
         ],
       ),
+    );
+  }
+}
+
+class HeroWidget extends StatelessWidget {
+  const HeroWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      clipBehavior: Clip.none,
+      children: [
+        AspectRatio(
+          aspectRatio: 2 / 2.25,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue.shade100.withOpacity(.1),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter,
+                image: NetworkImage(
+                  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/A7CzMZBitf00BAtb1kJa50pWPgV.jpg",
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              HeroStats(stat: "Emotional"),
+              HeroStats(stat: "drama"),
+              HeroStats(stat: "family"),
+              HeroStats(stat: "thriller", isFinalStat: true),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: -50,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width -
+                MediaQuery.of(context).size.width / 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const HeroActions(
+                  icon: Icons.add,
+                  iconText: "My List",
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                          size: 36,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: Text(
+                            "Play",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const HeroActions(
+                  icon: Icons.add,
+                  iconText: "My List",
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
