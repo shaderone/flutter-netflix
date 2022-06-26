e9a48c80ef970e1514796a6cb38e7c519256a11c
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-26 15:17:37 +0530

    Create new&hot appBar with tabBar

--- lib/presentation/pages/new_and_hot/new_and_hot_screen.dart
+++ lib/presentation/pages/new_and_hot/new_and_hot_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_clone/core/colors.dart';
+import 'package:netflix_clone/presentation/pages/new_and_hot/synced_appbar.dart';
 import 'package:netflix_clone/presentation/root/widgets/bottom_nav.dart';
 
 class NewAndHotScreen extends StatelessWidget {
@@ -6,11 +9,33 @@ class NewAndHotScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("New and hot page"),
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(120),
+          child: SyncedAppbar(
+            title: "New & Hot",
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            _buildComingSoon(context),
+            _buildEveryonesWatching(),
+          ],
+        ),
+        bottomNavigationBar: const BottomNavWidget(),
       ),
-      bottomNavigationBar: BottomNavWidget(),
     );
+  }
+
+  Widget _buildComingSoon(context) {
+    //final screenDimension = MediaQuery.of(context).size;
+    return ListView();
+  }
+
+  Widget _buildEveryonesWatching() {
+    return ListView();
   }
 }
