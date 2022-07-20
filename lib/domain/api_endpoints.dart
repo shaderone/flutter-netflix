81fcb1838e8f9e26cfe048cf09894dc5dd826461
pixiedust07 <sharonshaji127001@gmail.com>
2022-07-20 21:21:28 +0530

    Define Possible errors and api calls

--- lib/domain/api_endpoints.dart
+++ lib/domain/api_endpoints.dart
@@ -2,5 +2,5 @@ import 'package:netflix_clone/core/strings.dart';
 import 'package:netflix_clone/infrastructure/api_key.dart';
 
 class ApiEndpoints {
-  static const downloads = "$baseURL/trending/all/day?api_key=$apiKey";
+  static const downloads = "${baseURL}trending/all/day?api_key=$apiKey";
 }
