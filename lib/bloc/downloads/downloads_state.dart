d2f310cf50d969c943b6c8bcdf5144742bdce1b7
pixiedust07 <sharonshaji127001@gmail.com>
2022-07-21 22:47:13 +0530

    Implement bloc on downloads page, define api call

---
+++ lib/bloc/downloads/downloads_state.dart
@@ -0,0 +1,23 @@
+import 'package:netflix_clone/domain/common_failures/main_failure.dart';
+import 'package:netflix_clone/domain/pages/downloads/modals/downloads_modal.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'downloads_state.freezed.dart';
+
+@freezed
+class DownloadsState with _$DownloadsState {
+  const factory DownloadsState({
+    required bool isLoading,
+    List<Downloads>? downloads,
+    required Option<Either<MainFailure, List<Downloads>>>
+        downloadsFailureOrSuccessOption,
+  }) = _DownloadsState;
+
+  //initial state of the downloadspage on app start
+  factory DownloadsState.initial() {
+    return DownloadsState(
+      isLoading: false,
+      downloadsFailureOrSuccessOption: none(),
+    );
+  }
+}
