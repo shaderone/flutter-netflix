f09a906a34d4d6979f2b08de8eb203a2270cf278
pixiedust07 <sharonshaji127001@gmail.com>
2022-07-29 21:42:13 +0530

    Add downloads-service

--- lib/domain/common_failures/main_failure.dart
+++ lib/domain/common_failures/main_failure.dart
@@ -3,6 +3,11 @@ part 'main_failure.freezed.dart';
 
 @freezed
 class MainFailure with _$MainFailure {
+  /*
+    factory keyword is used to 
+    identify a default or named constructor
+    implement constructors that do not produce new instances of an existing class.
+  */
   const factory MainFailure.clientFailure() = _ClientFailure;
   const factory MainFailure.serverFailure() = _ServerFailure;
 }
